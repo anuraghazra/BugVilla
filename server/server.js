93e141b8de1c35fbec99923de7bb2d8830b07b99
anuraghazra <hazru.anurag@gmail.com>
2019-12-23 20:27:10 +0530

    fix: added security header / packages

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+
 const httpResponder = require('./middleware/httpResponder');
+const mongoSanitize = require('express-mongo-sanitize');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
 
 const PORT = 5000;
 
@@ -14,19 +18,24 @@ const bugsRoute = require('./routes/bugsRoute');
 const commentsRoute = require('./routes/commentsRoute');
 
 // connect to database
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_CONNECT_ATLAS, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to database')
   })
   .catch(() => console.log("Failed to connect to database."));
 
+// express settings
 app.set("env", process.env.NODE_ENV);
 
 // middlewares
-app.use(httpResponder);
+app.use(helmet()) // security headers
 app.use(cors());
+app.use(httpResponder);
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10kb' }));
+app.use(mongoSanitize()); // sanitization against NoSQL Injection Attacks
+app.use(xss()); // sanitize data
+
 
 // routes
 app.use('/api/user', userRoute);
