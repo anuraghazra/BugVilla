f8430416ef893351d2c90799525f8df83a6aa830
anuraghazra <hazru.anurag@gmail.com>
2020-01-20 20:10:49 +0530

    chore(tests): added more tests!

--- client/cypress/integration/dashboard.spec.ts
+++ client/cypress/integration/dashboard.spec.ts
@@ -1,21 +1,72 @@
 /// <reference types="cypress" />
 
-describe('Should Login', () => {
+describe('Test Dashboard', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
       .findByText(/Already have an account\?/i)
       .click()
-  })
-
-  it('Login user', function () {
-    cy.findByPlaceholderText(/example@gmail\.com/i)
+      .findByPlaceholderText(/example@gmail\.com/i)
       .focus()
       .type('newuser@gmail.com{enter}123456789{enter}')
-      .findByText(/Track Bugs/i).get('.bug__title')
+  })
+
+
+  it('should go to single bug', function () {
+    cy.findByText(/Track Bugs/i).get('.bug__title')
       .first()
       .click()
       .wait(1000)
-      .findByText(/^Opened by/)
+    cy.url().should('include', '/dashboard/bugs/')
+  })
+
+  it('should add a new bug', () => {
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: '/api/bugs',
+      response: []
+    })
+    cy.findByText(/Add/i)
+      .click()
+      .findByText(/Submit new bug/i)
+      .wait(500)
+      .findByPlaceholderText(/Enter Title/i)
+      .click()
+      .type('Cypres testing')
+      .findByPlaceholderText(/write markdown/i)
+      .click()
+      .type('## Cypres testing\n **hello cypress**')
+      .findByText(/Preview/i).click()
+      .findByText(/Write/i).click()
+      .findByText(/submit/i, { selector: 'button' })
+      .click()
+  })
+
+  it('should add a new comment', () => {
+    cy.server();
+    cy.route('PATCH', '/api/bugs/*/comments', 'fixture:add-comment.json')
+    cy.findByText(/Cypres testing/i, { selector: 'h3' })
+      .click()
+      .wait(500)
+      .findByPlaceholderText(/write markdown/i)
+      .click()
+      .type('### Hello Cypres comment')
+      .findByText(/Preview/i).click()
+      .findByText(/Write/i).click()
+      .findByText(/comment/i, { selector: 'button' })
+      .click()
+  })
+
+  it('should add/remove bug labels', () => {
+    cy.server();
+    cy.route('PATCH', '/api/bugs/*/labels', { "data": ["bug", "feature"] })
+    cy.findByText(/Cypres testing/i, { selector: 'h3' })
+      .click()
+      .wait(500)
+      .get('.label__header>svg').click()
+      .get('.dropdown__items')
+      .findByText(/feature/i).click()
+      .findByText(/update labels/i).click()
   })
 
 })
