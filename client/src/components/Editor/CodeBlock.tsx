33a0660f2c811c44cac83bf5b32581152bdbfe7c
anuraghazra <hazru.anurag@gmail.com>
2020-02-13 21:33:17 +0530

    chore: wip CodeBlock cleanup

--- client/src/components/Editor/CodeBlock.tsx
+++ client/src/components/Editor/CodeBlock.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { githubGist as theme } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
-import md from 'react-syntax-highlighter/dist/esm/languages/hljs/markdown';
-import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { prism as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
+// import md from 'react-syntax-highlighter/dist/esm/languages/hljs/markdown';
+// import html from 'react-syntax-highlighter/dist/esm/languages/hljs/htmlbars';
+// import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
+import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
+
+// SyntaxHighlighter.registerLanguage('md', md);
+// SyntaxHighlighter.registerLanguage('html', html);
+// SyntaxHighlighter.registerLanguage('css', css);
 SyntaxHighlighter.registerLanguage('javascript', js);
-SyntaxHighlighter.registerLanguage('md', md);
-SyntaxHighlighter.registerLanguage('css', css);
+SyntaxHighlighter.registerLanguage('jsx', jsx);
 
 interface CodeBlockProps {
   language?: string;
@@ -16,7 +21,7 @@ interface CodeBlockProps {
 
 const CodeBlock: React.FC<CodeBlockProps> = ({ language, value = '' }) => {
   if (!language) language = 'text';
-
+  console.log({language})
   return (
     <SyntaxHighlighter language={language} style={theme}>
       {value}
