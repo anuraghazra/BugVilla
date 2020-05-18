eb4e6307fce61407d4b7a98d848c280b667a7961
anuraghazra <hazru.anurag@gmail.com>
2020-05-18 13:03:17 +0530

    feat: added pagination

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -4,11 +4,14 @@ const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const sgMail = require('@sendgrid/mail');
 
-const { User, validateUser, validateUserLogin } = require('../models/userModel');
+const {
+  User,
+  validateUser,
+  validateUserLogin,
+} = require('../models/userModel');
 const { Bug } = require('../models/bugModel');
 const { Token } = require('../models/tokenModel');
-const { extractUsernameFromEmail } = require('../utils')
-
+const { extractUsernameFromEmail } = require('../utils');
 
 /**
  * @route POST /user/signup
@@ -17,7 +20,7 @@ const { extractUsernameFromEmail } = require('../utils')
 exports.signup = async (req, res) => {
   const { error, value } = validateUser(req.body);
   if (error) {
-    return res.unprocessable({ error: error.details[0].message })
+    return res.unprocessable({ error: error.details[0].message });
   }
 
   // save the user data into database
@@ -26,12 +29,13 @@ exports.signup = async (req, res) => {
 
   try {
     const foundUser = await User.findOne({
-      'email': value.email
+      email: value.email,
     });
-    if (foundUser) return res.conflict({ error: "Username / Email Already Exists" });
+    if (foundUser)
+      return res.conflict({ error: 'Username / Email Already Exists' });
 
     if (!req.file) {
-      return res.unprocessable({ error: "Please Select An Image" })
+      return res.unprocessable({ error: 'Please Select An Image' });
     }
 
     // save the user data into database
@@ -43,20 +47,23 @@ exports.signup = async (req, res) => {
       email: value.email,
       password: value.password,
       avatar: req.file.id,
-      avatarUrl: `http://${req.headers.host}/api/user/${usernameFromEmail}/avatar/raw`
+      avatarUrl: `http://${req.headers.host}/api/user/${usernameFromEmail}/avatar/raw`,
     });
     const savedUser = await newUser.save();
 
     // create email verification token
     let token = new Token({
       _userId: savedUser._id,
-      token: crypto.randomBytes(16).toString('hex')
+      token: crypto.randomBytes(16).toString('hex'),
     });
 
     const savedToken = await token.save();
-    if (!savedToken) return res.internalError({ error: 'Something wen\'t wrong while verifying email' })
+    if (!savedToken)
+      return res.internalError({
+        error: "Something wen't wrong while verifying email",
+      });
     // create verification link and send email
-    const verificationLink = `http://${req.headers.host}/api/user/verify-email?token=${token.token}`
+    const verificationLink = `http://${req.headers.host}/api/user/verify-email?token=${token.token}`;
     const msg = {
       to: savedUser.email,
       from: 'bugvilla@gmail.com',
@@ -64,8 +71,8 @@ exports.signup = async (req, res) => {
       templateId: 'd-110786e4fc3e4ce2b216b16e4ae73efd',
       dynamic_template_data: {
         user: savedUser.name,
-        verification_link: verificationLink
-      }
+        verification_link: verificationLink,
+      },
     };
     sgMail.send(msg);
 
@@ -77,17 +84,16 @@ exports.signup = async (req, res) => {
         email: savedUser.email,
         username: savedUser.username,
         name: savedUser.name,
-        message: "User Registered",
-      }
+        message: 'User Registered',
+      },
     });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
-
+};
 
 /**
  * @route POST /user/login
@@ -96,39 +102,47 @@ exports.signup = async (req, res) => {
 exports.login = async (req, res) => {
   const { error, value } = validateUserLogin(req.body);
   if (error) {
-    return res.unprocessable({ error: error.details[0].message })
+    return res.unprocessable({ error: error.details[0].message });
   }
 
   try {
     // check if user exist
     const user = await User.findOne({ email: value.email });
-    if (!user) return res.notFound({ error: "Email does not exists" });
+    if (!user) return res.notFound({ error: 'Email does not exists' });
 
     // make sure user is verified
-    if (!user.isVerified) return res.forbidden({ error: 'Email not verified' })
+    if (!user.isVerified) return res.forbidden({ error: 'Email not verified' });
 
     // user only signed up with google
     if (!user.password || !user.provider.includes('local')) {
-      return res.notFound({ error: 'Unknown auth method, Try logging in with Google' })
+      return res.notFound({
+        error: 'Unknown auth method, Try logging in with Google',
+      });
     }
 
     // Check/Compares password
     const validPassword = await user.isValidPassword(value.password);
-    if (!validPassword) return res.forbidden({ error: "Password is incorrect" })
+    if (!validPassword)
+      return res.forbidden({ error: 'Password is incorrect' });
 
     // Create JWT Token
-    const token = jwt.sign({
-      sub: user.id,
-      isVerified: user.isVerified,
-      username: user.username,
-      provider: user.provider,
-      name: user.name,
-      email: user.email,
-      avatarUrl: user.avatarUrl,
-      id: user.id
-    }, process.env.TOKEN_SECRET, { expiresIn: '2h' });
+    const token = jwt.sign(
+      {
+        sub: user.id,
+        isVerified: user.isVerified,
+        username: user.username,
+        provider: user.provider,
+        name: user.name,
+        email: user.email,
+        avatarUrl: user.avatarUrl,
+        id: user.id,
+      },
+      process.env.TOKEN_SECRET,
+      { expiresIn: '2h' }
+    );
 
-    res.status(200)
+    res
+      .status(200)
       .cookie('jwt', token, { maxAge: 2 * 3600000, httpOnly: true })
       .send({
         data: {
@@ -138,15 +152,15 @@ exports.login = async (req, res) => {
           name: user.name,
           email: user.email,
           avatarUrl: user.avatarUrl,
-          id: user.id
-        }
+          id: user.id,
+        },
       });
   } catch (err) {
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
 
 /**
  * @route POST /user/logout
@@ -155,8 +169,8 @@ exports.login = async (req, res) => {
 exports.logout = (req, res) => {
   req.logout();
   // res.send(req.user)
-  res.status(200).clearCookie('jwt').send({ message: 'logged out' })
-}
+  res.status(200).clearCookie('jwt').send({ message: 'logged out' });
+};
 
 /**
  * @route PATCH /user/me/bio
@@ -164,41 +178,42 @@ exports.logout = (req, res) => {
  */
 exports.updateBio = async (req, res) => {
   const { error, value } = Joi.object({
-    bio: Joi.string().max(100).required()
+    bio: Joi.string().max(100).required(),
   }).validate(req.body);
 
   if (error) {
-    return res.unprocessable({ error: error.details[0].message })
+    return res.unprocessable({ error: error.details[0].message });
   }
 
   try {
-
-    let user = await User.findOneAndUpdate({
-      _id: req.user.id
-    }, {
-      bio: value.bio
-    }, { new: true });
+    let user = await User.findOneAndUpdate(
+      {
+        _id: req.user.id,
+      },
+      {
+        bio: value.bio,
+      },
+      { new: true }
+    );
 
     if (!user) return res.notFound({ error: 'User not found' });
 
-    res.ok({ data: user.bio })
+    res.ok({ data: user.bio });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong while updating bio'
+      error: 'Something went wrong while updating bio',
     });
   }
-}
-
+};
 
 /**
  * @route GET /user/check-status
  * @type RequestHandler
  */
 exports.checkAuth = (req, res) => {
-  res.ok({ data: req.user })
-}
-
+  res.ok({ data: req.user });
+};
 
 /**
  * @route POST /user/verify-email
@@ -208,12 +223,17 @@ exports.verifyEmail = async (req, res) => {
   try {
     // find token
     let token = await Token.findOne({ token: req.query.token });
-    if (!token) return res.notFound({ error: 'Unable to find verification token' })
+    if (!token)
+      return res.notFound({ error: 'Unable to find verification token' });
 
     // find user with matching token
     let user = await User.findOne({ _id: token._userId });
-    if (!user) return res.notFound({ error: 'Unable to find matching user & token for verification' })
-    if (user.isVerified) return res.badRequest({ error: 'User is already verified' })
+    if (!user)
+      return res.notFound({
+        error: 'Unable to find matching user & token for verification',
+      });
+    if (user.isVerified)
+      return res.badRequest({ error: 'User is already verified' });
 
     // everything looks good! save user
     user.isVerified = true;
@@ -221,21 +241,21 @@ exports.verifyEmail = async (req, res) => {
     user.expires = null;
 
     let savedUser = await user.save();
-    if (!savedUser) return res.internalError({ error: 'Error while verifying user' })
+    if (!savedUser)
+      return res.internalError({ error: 'Error while verifying user' });
 
     // res.ok({
     //   data: savedUser,
     //   message: 'Email address successfully verified.'
     // })
-    res.redirect('/')
+    res.redirect('/');
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong while verifying email address'
+      error: 'Something went wrong while verifying email address',
     });
   }
-}
-
+};
 
 /**
  * @route GET /user/:username
@@ -243,17 +263,22 @@ exports.verifyEmail = async (req, res) => {
  */
 exports.getByUsername = async (req, res) => {
   try {
-    let user = await User.findOne({ username: req.params.username }).select('-password');
-    if (!user) return res.notFound({ error: `User not found with the username ${req.params.username}` })
+    let user = await User.findOne({ username: req.params.username }).select(
+      '-password'
+    );
+    if (!user)
+      return res.notFound({
+        error: `User not found with the username ${req.params.username}`,
+      });
 
     res.ok({ data: user });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
 
 /**
  * @route GET /user/:user_id
@@ -261,47 +286,56 @@ exports.getByUsername = async (req, res) => {
  */
 exports.getMultipleByIds = async (req, res) => {
   let { error, value } = Joi.object({
-    user_ids: Joi.array().items(Joi.string()).required()
+    user_ids: Joi.array().items(Joi.string()).required(),
   }).validate(req.body);
 
   if (error) {
-    return res.unprocessable({ error: error.details[0].message })
+    return res.unprocessable({ error: error.details[0].message });
   }
 
   try {
     let user = await User.find({
-      '_id': {
-        $in: [...value.user_ids]
-      }
-    }).select('username')
-    if (!user) return res.notFound({ error: `Users not found` })
+      _id: {
+        $in: [...value.user_ids],
+      },
+    }).select('username');
+    if (!user) return res.notFound({ error: `Users not found` });
 
     res.ok({ data: user });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
 
 /**
- * @route GET /user/
+ * @route GET /user/:page
  * @type RequestHandler
  */
 exports.getAllUsers = async (req, res) => {
+  const MAX_ITEMS = 10;
+  const page = parseInt(req.query.page - 1);
   try {
-    let users = await User.find({}).select('-password -email').sort({ name: 1 });
-    if (!users) return res.notFound({ error: `No users found!` })
+    let users = await User.find({})
+      .select('-password -email')
+      .sort('date_joined');
 
-    res.ok({ data: users });
+    if (!users) return res.notFound({ error: `No users found!` });
+
+    res.ok({
+      totalDocs: users.length,
+      totalPages: Math.ceil(users.length / MAX_ITEMS),
+      data: users.slice(MAX_ITEMS * page, MAX_ITEMS * page + MAX_ITEMS),
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong while getting users'
+      error: 'Something went wrong while getting users',
     });
   }
-}
+};
 
 /**
  * @route GET /user/me
@@ -309,16 +343,16 @@ exports.getAllUsers = async (req, res) => {
  */
 exports.getCurrent = async (req, res) => {
   try {
-    let user = await User.findOne({ _id: req.user.id }).select('-password')
-    if (!user) return res.notFound({ error: "User Not Found!" })
+    let user = await User.findOne({ _id: req.user.id }).select('-password');
+    if (!user) return res.notFound({ error: 'User Not Found!' });
 
     res.ok({ data: user });
   } catch (err) {
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
 
 /**
  * @route GET /user/:username/comments
@@ -338,23 +372,23 @@ exports.getCommentsByUser = async (req, res) => {
           author: {
             id: '$comments.author._id',
             username: '$comments.author.username',
-            name: '$comments.author.name'
+            name: '$comments.author.name',
           },
-          'id': '$_id',
+          id: '$_id',
           _id: 0,
         },
       },
-    ])
+    ]);
 
-    if (!bug) return res.notFound({ error: "Bug Not Found!" })
+    if (!bug) return res.notFound({ error: 'Bug Not Found!' });
     res.ok({ data: bug });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
 
 /**
  * @route GET /user/:username/reactions/count
@@ -368,19 +402,19 @@ exports.getCommentsCountByUser = async (req, res) => {
       { $unwind: '$comments' },
       { $match: { 'comments.author.username': req.params.username } },
       {
-        $count: "counts"
-      }
-    ])
+        $count: 'counts',
+      },
+    ]);
 
-    if (!data) return res.notFound({ error: "Not Found!" })
+    if (!data) return res.notFound({ error: 'Not Found!' });
     res.ok({ data: data[0] || { count: '0' } });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
 
 /**
  * @route GET /user/:username/reactions/count
@@ -395,7 +429,12 @@ exports.getCollectedReactionsCount = async (req, res) => {
       { $match: { 'comments.author.username': req.params.username } },
       { $project: { reactions: '$comments.reactions' } },
       { $unwind: '$reactions' },
-      { $group: { _id: '$reactions.emoji', reactions: { $push: '$reactions.users' } } },
+      {
+        $group: {
+          _id: '$reactions.emoji',
+          reactions: { $push: '$reactions.users' },
+        },
+      },
       // flatten multidimensional array
       // https://stackoverflow.com/a/41634661/10629172
       {
@@ -404,24 +443,24 @@ exports.getCollectedReactionsCount = async (req, res) => {
           _id: 0,
           users: {
             $map: {
-              input: "$reactions",
-              as: "itemList",
-              in: { $arrayElemAt: ["$$itemList", 0] }
-            }
-          }
-        }
-      }
-    ])
+              input: '$reactions',
+              as: 'itemList',
+              in: { $arrayElemAt: ['$$itemList', 0] },
+            },
+          },
+        },
+      },
+    ]);
 
-    if (!data) return res.notFound({ error: "Not Found!" })
+    if (!data) return res.notFound({ error: 'Not Found!' });
     res.ok({ data: data });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
 
 /**
  * @route GET /user/:username/bugs
@@ -429,14 +468,14 @@ exports.getCollectedReactionsCount = async (req, res) => {
  */
 exports.getBugsByUser = async (req, res) => {
   try {
-    let bug = await Bug.find({ 'author.username': req.params.username })
-    if (!bug) return res.notFound({ error: "Bug Not Found!" })
+    let bug = await Bug.find({ 'author.username': req.params.username });
+    if (!bug) return res.notFound({ error: 'Bug Not Found!' });
 
     res.ok({ data: bug });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.internalError({
-      error: 'Something went wrong'
+      error: 'Something went wrong',
     });
   }
-}
+};
--- server/routes/notificationsRoute.js
+++ server/routes/notificationsRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const NotificationController = require('../controllers/NotificationController');
 
-router.get('/', NotificationController.getNotifications)
-router.post('/mentions/:bugId', NotificationController.mentionPeople)
+router.get('/', NotificationController.getNotifications);
+router.post('/mentions/:bugId', NotificationController.mentionPeople);
 
 module.exports = router;
